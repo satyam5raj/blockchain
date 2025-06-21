b3238e182f1ec7e5b1291ed8c0329605643803ce
satyam5raj <satyamraj529@gmail.com>
2025-06-21 11:52:39 +0530

    basic blockchsin

---
+++ blockchain.js
@@ -0,0 +1,47 @@
+import SHA256 from 'crypto-js/sha256';
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, Date.now(), "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const curr = this.chain[i];
+      const prev = this.chain[i - 1];
+
+      if (curr.hash !== curr.calculateHash() || curr.previousHash !== prev.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
